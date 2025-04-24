ccabcde5e7f3dbb84916bc37cda6a1cd2510f2bb
Matt Lacey <matt@mrlacey.co.uk>
2025-04-24 07:56:56 +0100

    improved deleted method detection

--- MyGithubActionBot.Tests/UnitTest1.cs
+++ MyGithubActionBot.Tests/UnitTest1.cs
@@ -65,7 +65,24 @@ namespace MyGithubActionBot.Tests
 
         [Theory]
         [InlineData("- public void MyMethod()", true, "MyMethod")]
+        [InlineData("- public static void MyMethod2()", true, "MyMethod2")]
+        [InlineData("- public async void MyMethod3()", true, "MyMethod3")]
+        [InlineData("- public static async MyMethod4()", true, "MyMethod4")]
         [InlineData("public void MyMethod()", false, "")]
+        [InlineData("    void MyMethod()", false, "")]
+        [InlineData("    private MyMethod()", false, "")]
+        [InlineData("+ public void MyMethod()", false, "")]
+        [InlineData("+ public static void MyMethod2()", false, "")]
+        [InlineData("+ public async void MyMethod3()", false, "")]
+        [InlineData("+ public static async MyMethod4()", false, "")]
+        [InlineData("- private void MyMethod()", false, "")]
+        [InlineData("- private static void MyMethod2()", false, "")]
+        [InlineData("- private async void MyMethod3()", false, "")]
+        [InlineData("- private static async MyMethod4()", false, "")]
+        [InlineData("- protected void MyMethod()", false, "")]
+        [InlineData("- protected static void MyMethod2()", false, "")]
+        [InlineData("- protected async void MyMethod3()", false, "")]
+        [InlineData("- protected static async MyMethod4()", false, "")]
         public void TestDeletedPublicMethodRegex_ValidValues(string input, bool isMatchExpected, string expectedMethodName)
         {
             var regex = new Regex(Program.DeletedPublicMethodRegex);
